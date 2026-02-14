5d0afa48bad4e428950b123aa1e64dd054c3bfaf
Edmond <1571649+EdmondDantes@users.noreply.github.com>
2026-02-14 12:54:28 +0200

    Add \Cancellation as a new Throwable root class
    Introduce \Cancellation alongside \Exception and \Error as a third
    root class implementing Throwable. This allows async cancellation
    exceptions to avoid accidental suppression by catch(\Exception) or
    catch(\Error) blocks, per the True Async RFC.
    
    Changes:
    - Added Cancellation class with identical property layout and
      implementation-aliased methods (same pattern as Error)
    - Updated zend_implement_throwable() to accept Cancellation root
    - Updated i_get_exception_base() for 3-way dispatch
    - Registered zend_ce_cancellation in zend_register_default_exception()

--- Zend/zend_exceptions.stub.php
+++ Zend/zend_exceptions.stub.php
@@ -139,6 +139,64 @@ class Error implements Throwable
     public function __toString(): string {}
 }
 
+class Cancellation implements Throwable
+{
+    /**
+     * Intentionally left untyped for BC reasons
+     * @var string
+     */
+    protected $message = "";
+    private string $string = "";
+    /**
+     * Intentionally left untyped for BC reasons
+     * @var int
+     */
+    protected $code = 0;
+    protected string $file = "";
+    protected int $line = 0;
+    private array $trace = [];
+    private ?Throwable $previous = null;
+
+    /** @implementation-alias Exception::__clone */
+    private function __clone(): void {}
+
+    /** @implementation-alias Exception::__construct */
+    public function __construct(string $message = "", int $code = 0, ?Throwable $previous = null) {}
+
+    /**
+     * @tentative-return-type
+     * @implementation-alias Exception::__wakeup
+     */
+    public function __wakeup(): void {}
+
+    /** @implementation-alias Exception::getMessage */
+    final public function getMessage(): string {}
+
+    /**
+     * @return int
+     * @implementation-alias Exception::getCode
+     */
+    final public function getCode() {}
+
+    /** @implementation-alias Exception::getFile */
+    final public function getFile(): string {}
+
+    /** @implementation-alias Exception::getLine */
+    final public function getLine(): int {}
+
+    /** @implementation-alias Exception::getTrace */
+    final public function getTrace(): array {}
+
+    /** @implementation-alias Exception::getPrevious */
+    final public function getPrevious(): ?Throwable {}
+
+    /** @implementation-alias Exception::getTraceAsString */
+    final public function getTraceAsString(): string {}
+
+    /** @implementation-alias Exception::__toString */
+    public function __toString(): string {}
+}
+
 class CompileError extends Error
 {
 }
--- Zend/zend_exceptions_arginfo.h
+++ Zend/zend_exceptions_arginfo.h
@@ -81,6 +81,28 @@ ZEND_END_ARG_INFO()
 
 #define arginfo_class_Error___toString arginfo_class_Throwable_getMessage
 
+#define arginfo_class_Cancellation___clone arginfo_class_Exception___clone
+
+#define arginfo_class_Cancellation___construct arginfo_class_Exception___construct
+
+#define arginfo_class_Cancellation___wakeup arginfo_class_Exception___wakeup
+
+#define arginfo_class_Cancellation_getMessage arginfo_class_Throwable_getMessage
+
+#define arginfo_class_Cancellation_getCode arginfo_class_Throwable_getCode
+
+#define arginfo_class_Cancellation_getFile arginfo_class_Throwable_getMessage
+
+#define arginfo_class_Cancellation_getLine arginfo_class_Throwable_getLine
+
+#define arginfo_class_Cancellation_getTrace arginfo_class_Throwable_getTrace
+
+#define arginfo_class_Cancellation_getPrevious arginfo_class_Throwable_getPrevious
+
+#define arginfo_class_Cancellation_getTraceAsString arginfo_class_Throwable_getMessage
+
+#define arginfo_class_Cancellation___toString arginfo_class_Throwable_getMessage
+
 ZEND_METHOD(Exception, __clone);
 ZEND_METHOD(Exception, __construct);
 ZEND_METHOD(Exception, __wakeup);
@@ -139,6 +161,21 @@ static const zend_function_entry class_Error_methods[] = {
 	ZEND_RAW_FENTRY("getPrevious", zim_Exception_getPrevious, arginfo_class_Error_getPrevious, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
 	ZEND_RAW_FENTRY("getTraceAsString", zim_Exception_getTraceAsString, arginfo_class_Error_getTraceAsString, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
 	ZEND_RAW_FENTRY("__toString", zim_Exception___toString, arginfo_class_Error___toString, ZEND_ACC_PUBLIC, NULL, NULL)
+	ZEND_FE_END
+};
+
+static const zend_function_entry class_Cancellation_methods[] = {
+	ZEND_RAW_FENTRY("__clone", zim_Exception___clone, arginfo_class_Cancellation___clone, ZEND_ACC_PRIVATE, NULL, NULL)
+	ZEND_RAW_FENTRY("__construct", zim_Exception___construct, arginfo_class_Cancellation___construct, ZEND_ACC_PUBLIC, NULL, NULL)
+	ZEND_RAW_FENTRY("__wakeup", zim_Exception___wakeup, arginfo_class_Cancellation___wakeup, ZEND_ACC_PUBLIC, NULL, NULL)
+	ZEND_RAW_FENTRY("getMessage", zim_Exception_getMessage, arginfo_class_Cancellation_getMessage, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
+	ZEND_RAW_FENTRY("getCode", zim_Exception_getCode, arginfo_class_Cancellation_getCode, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
+	ZEND_RAW_FENTRY("getFile", zim_Exception_getFile, arginfo_class_Cancellation_getFile, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
+	ZEND_RAW_FENTRY("getLine", zim_Exception_getLine, arginfo_class_Cancellation_getLine, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
+	ZEND_RAW_FENTRY("getTrace", zim_Exception_getTrace, arginfo_class_Cancellation_getTrace, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
+	ZEND_RAW_FENTRY("getPrevious", zim_Exception_getPrevious, arginfo_class_Cancellation_getPrevious, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
+	ZEND_RAW_FENTRY("getTraceAsString", zim_Exception_getTraceAsString, arginfo_class_Cancellation_getTraceAsString, ZEND_ACC_PUBLIC|ZEND_ACC_FINAL, NULL, NULL)
+	ZEND_RAW_FENTRY("__toString", zim_Exception___toString, arginfo_class_Cancellation___toString, ZEND_ACC_PUBLIC, NULL, NULL)
 	ZEND_FE_END
 };
 
@@ -233,6 +270,46 @@ static zend_class_entry *register_class_Error(zend_class_entry *class_entry_Thro
 
 	zval property_line_default_value;
 	ZVAL_UNDEF(&property_line_default_value);
+	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_LINE), &property_line_default_value, ZEND_ACC_PROTECTED, NULL, (zend_type) ZEND_TYPE_INIT_MASK(MAY_BE_LONG));
+
+	zval property_trace_default_value;
+	ZVAL_EMPTY_ARRAY(&property_trace_default_value);
+	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_TRACE), &property_trace_default_value, ZEND_ACC_PRIVATE, NULL, (zend_type) ZEND_TYPE_INIT_MASK(MAY_BE_ARRAY));
+
+	zval property_previous_default_value;
+	ZVAL_NULL(&property_previous_default_value);
+	zend_string *property_previous_class_Throwable = zend_string_init("Throwable", sizeof("Throwable")-1, 1);
+	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_PREVIOUS), &property_previous_default_value, ZEND_ACC_PRIVATE, NULL, (zend_type) ZEND_TYPE_INIT_CLASS(property_previous_class_Throwable, 0, MAY_BE_NULL));
+
+	return class_entry;
+}
+
+static zend_class_entry *register_class_Cancellation(zend_class_entry *class_entry_Throwable)
+{
+	zend_class_entry ce, *class_entry;
+
+	INIT_CLASS_ENTRY(ce, "Cancellation", class_Cancellation_methods);
+	class_entry = zend_register_internal_class_with_flags(&ce, NULL, 0);
+	zend_class_implements(class_entry, 1, class_entry_Throwable);
+
+	zval property_message_default_value;
+	ZVAL_EMPTY_STRING(&property_message_default_value);
+	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_MESSAGE), &property_message_default_value, ZEND_ACC_PROTECTED, NULL, (zend_type) ZEND_TYPE_INIT_NONE(0));
+
+	zval property_string_default_value;
+	ZVAL_EMPTY_STRING(&property_string_default_value);
+	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_STRING), &property_string_default_value, ZEND_ACC_PRIVATE, NULL, (zend_type) ZEND_TYPE_INIT_MASK(MAY_BE_STRING));
+
+	zval property_code_default_value;
+	ZVAL_LONG(&property_code_default_value, 0);
+	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_CODE), &property_code_default_value, ZEND_ACC_PROTECTED, NULL, (zend_type) ZEND_TYPE_INIT_NONE(0));
+
+	zval property_file_default_value;
+	ZVAL_EMPTY_STRING(&property_file_default_value);
+	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_FILE), &property_file_default_value, ZEND_ACC_PROTECTED, NULL, (zend_type) ZEND_TYPE_INIT_MASK(MAY_BE_STRING));
+
+	zval property_line_default_value;
+	ZVAL_LONG(&property_line_default_value, 0);
 	zend_declare_typed_property(class_entry, ZSTR_KNOWN(ZEND_STR_LINE), &property_line_default_value, ZEND_ACC_PROTECTED, NULL, (zend_type) ZEND_TYPE_INIT_MASK(MAY_BE_LONG));
 
 	zval property_trace_default_value;
