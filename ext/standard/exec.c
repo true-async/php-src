c142a5cf99ebb58c844459851d1eeb5e1950ec18
Edmond <1571649+edmonddantes@users.noreply.github.com>
2026-03-06 08:17:11 +0000

    Fix exec functions ignoring virtual CWD in async mode
    ZEND_ASYNC_EXEC was called with cwd=NULL, so child processes
    inherited the real process CWD instead of PHP's virtual CWD set
    by chdir(). Now pass VCWD_GETCWD result to ZEND_ASYNC_EXEC in
    php_exec() and shell_exec().

--- ext/standard/exec.c
+++ ext/standard/exec.c
@@ -125,8 +125,10 @@ PHPAPI int php_exec(int type, const char *cmd, zval *array, zval *return_value)
 
 #ifdef PHP_WIN32
 	if (ZEND_ASYNC_ON) {
+		char cur_cwd[MAXPATHLEN];
+		const char *cwd = VCWD_GETCWD(cur_cwd, MAXPATHLEN);
 		ZEND_ASYNC_SCHEDULER_INIT();
-		pclose_return = ZEND_ASYNC_EXEC(type, cmd, array, return_value, NULL, NULL, NULL, 0);
+		pclose_return = ZEND_ASYNC_EXEC(type, cmd, array, return_value, NULL, cwd, NULL, 0);
 		php_clear_stat_cache(0, NULL, 0);
 		return pclose_return;
 	} else {
@@ -134,13 +136,15 @@ PHPAPI int php_exec(int type, const char *cmd, zval *array, zval *return_value)
 	}
 #else
 	if (ZEND_ASYNC_ON) {
+		char cur_cwd[MAXPATHLEN];
+		const char *cwd = VCWD_GETCWD(cur_cwd, MAXPATHLEN);
 #if PHP_SIGCHILD
 		if (sig_handler) {
 			signal(SIGCHLD, sig_handler);
 		}
 #endif
 		ZEND_ASYNC_SCHEDULER_INIT();
-		pclose_return = ZEND_ASYNC_EXEC(type, cmd, array, return_value, NULL, NULL, NULL, 0);
+		pclose_return = ZEND_ASYNC_EXEC(type, cmd, array, return_value, NULL, cwd, NULL, 0);
 		php_clear_stat_cache(0, NULL, 0);
 		return pclose_return;
 	} else {
@@ -534,6 +538,8 @@ PHP_FUNCTION(shell_exec)
 	}
 
 	if (ZEND_ASYNC_ON) {
+		char cur_cwd[MAXPATHLEN];
+		const char *cwd = VCWD_GETCWD(cur_cwd, MAXPATHLEN);
 		ZEND_ASYNC_SCHEDULER_INIT();
 		ZEND_ASYNC_EXEC(
 			ZEND_ASYNC_EXEC_MODE_SHELL_EXEC,
@@ -541,7 +547,7 @@ PHP_FUNCTION(shell_exec)
 			return_value,
 			NULL,
 			NULL,
-			NULL,
+			cwd,
 			NULL,
 			0
 		);
